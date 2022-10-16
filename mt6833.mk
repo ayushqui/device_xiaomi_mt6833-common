a48f5188deb890ab5633a2940edda34106b2bd64
Jabiyeff <cebiyevanar@gmail.com>
2022-10-16 18:34:14 +0400

    camellia: parts: Import XiaomiParts for refresh rate control
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- mt6833.mk
+++ mt6833.mk
@@ -430,6 +430,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
-
-PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/vendor_hals/vendor_hals.xml:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/vendor_hals/vendor_hals.xml
+# XiaomiParts
+PRODUCT_PACKAGES += \
+    XiaomiParts
